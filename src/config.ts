cd1ffda738e692c43493dfb52b43bb7c906e1000
Minh <hlminh2000@gmail.com>
2020-07-29 17:55:16 -0400

    :sparkles: adds arranger metadata initialization

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const PORT = Number(process.env.PORT) || 6060;
+export const ES_HOSTS = (process.env.ES_HOST || "http://localhost:9200").split(
+  ","
+);
+export const ARRANGER_PROJECT_ID = String(process.env.ARRANGER_PROJECT_ID);
+export const ARRANGER_COHORT_CENTRIC_INDEX = String(
+  process.env.ARRANGER_COHORT_CENTRIC_INDEX
+);
